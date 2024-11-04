92a42bda1ef5907fcc3ac7cbdd86940e0f67ef8a
Dennis <dennojunior@gmail.com>
2024-11-05 01:54:31 +0300

    updated the working hours and tested the site

--- Xiaomi/app.js
+++ Xiaomi/app.js
@@ -6,17 +6,17 @@ const PORT = 3000;
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Middleware to check working hours
+// Middleware to check the day and working hours
 function workingHours(req, res, next) {
   const currentDate = new Date();
-  const day = currentDate.getDay(); // 0 (Sun) to 6 (Sat)
+  const day = currentDate.getDay();
   const hour = currentDate.getHours();
 
   // Check if it's Monday to Friday and between 9 and 17
-  if (day >= 1 && day <= 5 && hour >= 0 && hour < 17) {
+  if (day >= 1 && day <= 5 && hour >= 9 && hour < 17) {
     next();
   } else {
-    res.render('error'); // Render the error page
+    res.render('error'); // Render the error page under views
   }
 }
 
@@ -40,7 +40,7 @@ app.get('/contact', (req, res) => {
   res.render('contact');
 });
 
-// Start the server
+// Starting the server under port 3000
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
